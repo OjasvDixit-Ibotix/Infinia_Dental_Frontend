57f4601eebf27e8d7ab9629808435b24c7690d53
OjasvDixit-Ibotix <ojas.dixit@ibotix.in>
2025-07-14 16:36:33 +0530

    Navbar created

---
+++ src/components/EmployeeRecords/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className="inline-flex h-10 px-4 sm:px-[12px] py-[9px] justify-center items-center gap-2 sm:gap-[8px] flex-shrink-0 rounded-[6px] border border-[#EFCD78] bg-white m-4 sm:m-6">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none">
+        <path d="M8.00065 12.6663L3.33398 7.99967L8.00065 3.33301" stroke="#444" strokeWidth="1.33" strokeLinecap="round" strokeLinejoin="round" />
+        <path d="M12.6673 8H3.33398" stroke="#444" strokeWidth="1.33" strokeLinecap="round" strokeLinejoin="round" />
+      </svg>
+      <span className="text-[#444] text-sm sm:text-[14px] leading-[20px] font-[Segoe UI Symbol]">
+        Back to Dashboard
+      </span>
+    </div>
+  );
+};
+
+export default Navbar;
