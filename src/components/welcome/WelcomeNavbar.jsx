8e9864a376c6123027f04165a7c329a61ea2f989
OjasvDixit-Ibotix <ojas.dixit@ibotix.in>
2025-07-14 16:35:19 +0530

    Welcome Navbar created

--- src/components/welcome/WelcomeNavbar.jsx
+++ src/components/welcome/WelcomeNavbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Searchicon from '../../assets/svgs/WelcomePage/Searchicon';
+import SearchBar from '../SearchBar';
 
 const WelcomeNavbar = () => {
   return (
-    <div className="flex flex-col items-start gap-4 rounded-[20px] py-4 px-2 w-full max-w-[1336px] mx-auto">
+    <div className="flex flex-col items-start gap-4 rounded-[20px] w-full max-w-[1336px] mx-auto">
       <div className="flex flex-col md:flex-row items-start md:items-center justify-between w-full gap-4">
         <div className="flex items-center gap-4">
           <div className="flex items-center justify-center w-10 h-10 rounded-[12px] bg-[#444]">
@@ -19,19 +20,9 @@ const WelcomeNavbar = () => {
           </div>
         </div>
       </div>
+      
 
-      <div className="w-full max-w-[672px]">
-        <div className="relative flex items-center h-[48px] rounded-[12px] border border-[#EFCD78] bg-[rgba(255,255,255,0.5)] w-full px-4 pl-10">
-          <div className="absolute left-[12px] top-[14px] w-5 h-5 text-gray-600">
-            <Searchicon />
-          </div>
-          <input
-            type="text"
-            placeholder="Search employees, reports, leaves..."
-            className="w-full bg-transparent border-none outline-none text-[#444] placeholder:text-[#444] placeholder:text-[14px] placeholder:font-[400] placeholder:leading-none placeholder:font-[Segoe UI Symbol]"
-          />
-        </div>
-      </div>
+    <SearchBar placeholder='Search employees, reports, leaves...'/>
 
       <div className="flex flex-wrap gap-2">
         {['Time Off', 'Handbook', 'Seminars', 'Forms', 'Lab Protocols'].map((item) => (
