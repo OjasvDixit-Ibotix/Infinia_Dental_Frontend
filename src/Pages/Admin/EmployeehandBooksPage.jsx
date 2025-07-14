4d9ced1ef602d97da1e56368ead2812472d9441b
OjasvDixit-Ibotix <ojas.dixit@ibotix.in>
2025-07-14 18:25:34 +0530

    emp hand book page created

---
+++ src/Pages/Admin/EmployeehandBooksPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import EmpHandBookHeader from '../../components/EmployeeHandBook/EmpHandBookFilter'
+import EmpHandBookFilter from '../../components/EmployeeHandBook/EmpHandBookFilter'
+
+const EmployeehandBooksPage = () => {
+  return (
+    <>
+    <EmpHandBookFilter/>
+    </>
+  )
+}
+
+export default EmployeehandBooksPage
