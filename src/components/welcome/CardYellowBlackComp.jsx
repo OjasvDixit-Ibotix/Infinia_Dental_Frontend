d615c1bf754eab3da09e776edafba4261d224bdd
OjasvDixit-Ibotix <ojas.dixit@ibotix.in>
2025-07-14 11:37:37 +0530

    card yellow black created of hr dashborad

---
+++ src/components/welcome/CardYellowBlackComp.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import CardYellowBlackWrap from './wrappers/CardYellowBlackWrap';
+import EmployehandBook from '../../assets/svgs/WelcomePage/EmployehandBook';
+import LeaveRequest from '../../assets/svgs/WelcomePage/LeaveRequest';
+import CheckAttendance from '../../assets/svgs/WelcomePage/CheckAttendance';
+import Products from '../../assets/svgs/WelcomePage/Products';
+import Promotions from '../../assets/svgs/WelcomePage/Promotions';
+import Forms from '../../assets/svgs/WelcomePage/forms';
+import Directory from '../../assets/svgs/WelcomePage/Directory';
+import SeminarEvent from '../../assets/svgs/WelcomePage/SeminarEvent';
+import EmployeDetails from '../../assets/svgs/WelcomePage/EmployeDetails';
+
+const CardYellowBlackComp = () => {
+  return (
+    <div className="flex flex-wrap justify-center sm:justify-start gap-4 sm:gap-6 md:gap-8 ">
+      <CardYellowBlackWrap logos={<EmployeDetails />} logodesc="Employee Details" bgColor="EFCD78" textColor="444" />
+      <CardYellowBlackWrap logos={<LeaveRequest />} logodesc="Leave Request" bgColor="444" textColor="FFF" />
+      <CardYellowBlackWrap logos={<CheckAttendance />} logodesc="Check Attendance" bgColor="EFCD78" textColor="444" />
+      <CardYellowBlackWrap logos={<EmployehandBook />} logodesc="Employee Handbook" bgColor="444" textColor="FFF" />
+      <CardYellowBlackWrap logos={<Products />} logodesc="Products" bgColor="EFCD78" textColor="444" />
+      <CardYellowBlackWrap logos={<SeminarEvent />} logodesc="Seminars & Events" bgColor="444" textColor="FFF" />
+      <CardYellowBlackWrap logos={<Promotions />} logodesc="Promotions" bgColor="EFCD78" textColor="444" />
+      <CardYellowBlackWrap logos={<Forms />} logodesc="Forms" bgColor="444" textColor="FFF" />
+      <CardYellowBlackWrap logos={<Directory />} logodesc="Directory" bgColor="EFCD78" textColor="444" />
+    </div>
+  );
+};
+
+export default CardYellowBlackComp;
