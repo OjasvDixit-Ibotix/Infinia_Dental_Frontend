f72993555c27003a74f754f288ffc136aec2a47f
OjasvDixit-Ibotix <ojas.dixit@ibotix.in>
2025-07-15 16:06:12 +0530

    Products card list done

---
+++ src/components/ProductCataloge/ProductsCardList.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ProductsCardListWrap from './wrapper/ProductsCardListWrap'
+import image from '../../assets/svgs/WelcomePage/demoImage.svg'
+const ProductsCardList = () => {
+  return (
+    <>
+    <div className='w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6' >
+   <ProductsCardListWrap name={'Zirconia Crown'} image={image} desc={'High-strength zirconia crown with excellent aesthetics and biocompatibility.'} 
+     price={'3,200'} noOf={'5'}
+   />
+    <ProductsCardListWrap name={'Zirconia Crown'} image={image} desc={'High-strength zirconia crown with excellent aesthetics and biocompatibility.'} 
+     price={'3,200'} noOf={'5'}
+   />
+    <ProductsCardListWrap name={'Zirconia Crown'} image={image} desc={'High-strength zirconia crown with excellent aesthetics and biocompatibility.'} 
+     price={'3,200'} noOf={'5'}
+   />
+    <ProductsCardListWrap name={'Zirconia Crown'} image={image} desc={'High-strength zirconia crown with excellent aesthetics and biocompatibility.'} 
+     price={'3,200'} noOf={'5'}
+   />
+    <ProductsCardListWrap name={'Zirconia Crown'} image={image} desc={'High-strength zirconia crown with excellent aesthetics and biocompatibility.'} 
+     price={'3,200'} noOf={'5'}
+   />
+    <ProductsCardListWrap name={'Zirconia Crown'} image={image} desc={'High-strength zirconia crown with excellent aesthetics and biocompatibility.'} 
+     price={'3,200'} noOf={'5'}
+   />
+   </div>
+    </>
+
+   
+  )
+}
+
+export default ProductsCardList
