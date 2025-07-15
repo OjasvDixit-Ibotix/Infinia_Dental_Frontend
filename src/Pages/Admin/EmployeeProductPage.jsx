b5f822671618594eaafa1beabd7739669139081b
OjasvDixit-Ibotix <ojas.dixit@ibotix.in>
2025-07-15 16:04:57 +0530

    Empl product page comp added

--- src/Pages/Admin/EmployeeProductPage.jsx
+++ src/Pages/Admin/EmployeeProductPage.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ProductHeaderComp from '../../components/ProductCataloge/ProductHeaderComp'
+import ProductsCardList from '../../components/ProductCataloge/ProductsCardList'
+import ProductResourceComp from '../../components/ProductCataloge/ProductResourceComp'
 
 const EmployeeProductPage = () => {
   return (
     <>
     <ProductHeaderComp/>
+    <div className='flex flex-col gap-9'>
+
+    <ProductsCardList/>
+    <ProductResourceComp/>
+    </div>
 
     </>
   )
