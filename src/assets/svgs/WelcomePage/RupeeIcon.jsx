abcb70be19bf2c9049986add1e6a1e31b5813c99
OjasvDixit-Ibotix <ojas.dixit@ibotix.in>
2025-07-15 16:04:06 +0530

    Svg added

---
+++ src/assets/svgs/WelcomePage/RupeeIcon.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const RupeeIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+  <path d="M4 2H12" stroke="#EFCD78" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M4 5.33301H12" stroke="#EFCD78" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M4 8.66699L9.66667 14.0003" stroke="#EFCD78" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M4 8.66699H6" stroke="#EFCD78" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M6 8.66667C10.4447 8.66667 10.4447 2 6 2" stroke="#EFCD78" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+  )
+}
+
+export default RupeeIcon
