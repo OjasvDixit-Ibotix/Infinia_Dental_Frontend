174256c7725a6d4f7c8deaf461581a3107932a67
OjasvDixit-Ibotix <ojas.dixit@ibotix.in>
2025-07-15 13:28:11 +0530

    employee product page created

---
+++ src/Pages/Admin/EmployeeProductPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ProductHeaderComp from '../../components/ProductCataloge/ProductHeaderComp'
+
+const EmployeeProductPage = () => {
+  return (
+    <>
+    <ProductHeaderComp/>
+
+    </>
+  )
+}
+
+export default EmployeeProductPage
