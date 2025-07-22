087b1258b5aa3b2b0849e748a30d3d186b9363b4
OjasvDixit-Ibotix <ojas.dixit@ibotix.in>
2025-07-22 16:23:02 +0530

    radio buttons added in sign up page for admin and emploee

--- src/components/SignUpCard.jsx
+++ src/components/SignUpCard.jsx
@@ -3,9 +3,12 @@ import { Link } from "react-router-dom";
 
 const SignUpCard = ({ onSwitch }) => {
   return (
-    <div className="w-full max-w-[420px] bg-white rounded-[28px] sm:rounded-[32px] shadow-[0_4px_35px_rgba(0,0,0,0.08)] px-4 sm:px-8 py-8 mx-auto">
-      <div className="flex justify-between items-center mb-6">
-        <h2 className="text-lg sm:text-xl  text-black  font-normal  font-[Poppins]  text-[20px]  leading-normal">Welcome!</h2>
+    <div className="w-full  max-w-[420px] bg-white rounded-[28px] sm:rounded-[32px] shadow-[0_4px_35px_rgba(0,0,0,0.08)] px-4 sm:px-8 py-7 mx-auto">
+         
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="text-lg sm:text-xl text-black font-normal font-[Poppins] text-[20px] leading-normal">
+          Welcome!
+        </h2>
         <p className="text-xs sm:text-sm text-[#ABABAB]">
           Have an account?{" "}
           <span
@@ -16,11 +19,53 @@ const SignUpCard = ({ onSwitch }) => {
           </span>
         </p>
       </div>
-      <h1 className="text-2xl sm:text-[34px]  font-[Poppins]  leading-tight mb-6 text-black font-poppins text-[55px] font-medium ">
+      <h1 className="text-2xl sm:text-[34px] font-[Poppins] leading-tight mb-4 text-black font-poppins text-[55px] font-medium ">
         Sign up
       </h1>
+       <div className="mb-4 flex gap-x-3 flex-row" >
+        <label className="  text-black font-poppins text-[16px] font-normal leading-normal block text-xs sm:text-sm">
+          Select Role 
+        </label>
+        <div className="flex items-center gap-x-6">
+          <div className="flex items-center">
+            <input
+              id="role-admin"
+              name="role"
+              type="radio"
+              value="admin"
+              className="h-4 w-4 text-[#2C2C2C] focus:ring-[#EFCD78] border-gray-300"
+            />
+            <label
+              htmlFor="role-admin"
+              className="ml-2 block text-xs sm:text-sm text-gray-900 cursor-pointer"
+            >
+              Admin
+            </label>
+          </div>
+          <div className="flex items-center">
+            <input
+              id="role-employee"
+              name="role"
+              type="radio"
+              value="employee"
+              defaultChecked
+              className="h-4 w-4 text-[#2C2C2C] focus:ring-[#EFCD78] border-gray-300"
+            />
+            <label
+              htmlFor="role-employee"
+              className="ml-2 block text-xs sm:text-sm text-gray-900 cursor-pointer"
+            >
+              Employee
+            </label>
+          </div>
+        </div>
+      </div>
+      
       <div className="mb-5">
-        <label htmlFor="email" className=" mb-2 text-black font-poppins text-[16px] font-normal leading-normal block text-xs sm:text-sm">
+        <label
+          htmlFor="email"
+          className=" mb-2 text-black font-poppins text-[16px] font-normal leading-normal block text-xs sm:text-sm"
+        >
           Enter your username or email address
         </label>
         <input
@@ -30,10 +75,13 @@ const SignUpCard = ({ onSwitch }) => {
           placeholder="Username or email address"
           className="w-full px-3 py-2 sm:px-4 sm:py-3 border border-[#C0C0C0] rounded-md outline-none focus:border-[#3b82f6] text-xs sm:text-sm"
         />
-      </div>
+      </div> 
       <div className="mb-4 flex flex-col sm:flex-row gap-2 sm:gap-4">
         <div className="w-full sm:w-1/2">
-          <label htmlFor="username" className="mb-2 text-black font-poppins text-[16px] font-normal leading-normal block text-xs sm:text-sm">
+          <label
+            htmlFor="username"
+            className="mb-2 text-black font-poppins text-[16px] font-normal leading-normal block text-xs sm:text-sm"
+          >
             User name
           </label>
           <input
@@ -45,7 +93,10 @@ const SignUpCard = ({ onSwitch }) => {
           />
         </div>
         <div className="w-full sm:w-1/2">
-          <label htmlFor="contact" className="mb-2 text-black font-poppins text-[16px] font-normal leading-normal block text-xs sm:text-sm">
+          <label
+            htmlFor="contact"
+            className="mb-2 text-black font-poppins text-[16px] font-normal leading-normal block text-xs sm:text-sm"
+          >
             Contact Number
           </label>
           <input
@@ -57,8 +108,13 @@ const SignUpCard = ({ onSwitch }) => {
           />
         </div>
       </div>
+
+     
       <div className="mb-4">
-        <label htmlFor="password" className="mb-2 text-black font-poppins text-[16px] font-normal leading-normal block text-xs sm:text-sm">
+        <label
+          htmlFor="password"
+          className="mb-2 text-black font-poppins text-[16px] font-normal leading-normal block text-xs sm:text-sm"
+        >
           Enter your Password
         </label>
         <input
