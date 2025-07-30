69b73726d582aa79b532d096f18a124f1d341b4a
OjasvDixit-Ibotix <ojas.dixit@ibotix.in>
2025-07-30 16:08:24 +0530

    react-caledar added employee leave request page

---
+++ src/assets/svgs/EmpRequestLeave/LeftArrowIcon.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LeftArrowIcon = () => {
+  return (
+   <svg xmlns="http://www.w3.org/2000/svg" width="6" height="16" viewBox="0 0 6 16" fill="none">
+  <path d="M4.80273 14.4062V14.4062C0.208588 11.1656 0.208587 4.35278 4.80273 1.11213V1.11213" stroke="#EFCD78" stroke-width="2" stroke-linecap="round"/>
+</svg>
+  )
+}
+
+export default LeftArrowIcon
---
+++ src/assets/svgs/EmpRequestLeave/RightArrowIcon.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const RightArrowIcon = () => {
+  return (
+  
+ <svg xmlns="http://www.w3.org/2000/svg" width="7" height="16" viewBox="0 0 7 16" fill="none">
+  <path d="M1.87305 1.11328V1.11328C6.46719 4.35393 6.46719 11.1668 1.87305 14.4074V14.4074" stroke="#EFCD78" stroke-width="2" stroke-linecap="round"/>
+</svg>
+  )
+}
+
+export default RightArrowIcon
