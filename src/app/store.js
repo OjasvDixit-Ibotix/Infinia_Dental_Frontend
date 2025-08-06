4bf552748e27612ec319ba53ea0f164092f06079
OjasvDixit-Ibotix <ojas.dixit@ibotix.in>
2025-08-06 20:50:52 +0530

    leaveslice reducer store created

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../slices/auth/authSlice'
-
+import leaveReducer from '../slices/leaveSlice'
 export const store = configureStore({
     reducer :{
         auth : authReducer,
+        leave: leaveReducer
     }
 }) 
 
